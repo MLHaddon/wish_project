5f3ee175348dcd3563f9df09870ad603675f9e96
MLHaddon <haddon.mike2@outlook.com>
2021-01-23 19:15:52 -0600

    update settings

--- wish_project/settings.py
+++ wish_project/settings.py
@@ -23,9 +23,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '25u)(f0!wi7f0+j4w!kzc_-r34$*qhpdj_vna#$30^wqjkvq5m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['3.136.156.43', '127.0.0.1']
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # Application definition
 
@@ -119,4 +122,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
